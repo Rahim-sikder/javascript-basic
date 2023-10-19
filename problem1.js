190b1b9249e0e9b216f464eb126586f177e28819
Rahim sikder <arsikder323@gmail.com>
2023-10-20 00:19:30 +0600

    problem si done

---
+++ problem1.js
@@ -0,0 +1,8 @@
+var fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+var findBanana = (fruits[1]);
+console.log(findBanana);
+fruits[1] = 'Mango';
+fruits.pop();
+fruits.push('watermelon');
+console.log(fruits);
