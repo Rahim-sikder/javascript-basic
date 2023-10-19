b041b7d4b34bd39005d556ddf86a8e6fe4cb4fdf
Rahim sikder <arsikder323@gmail.com>
2023-10-19 23:27:58 +0600

    multi stage condition clear

---
+++ multi-stage-condition.js
@@ -0,0 +1,18 @@
+var money = 100;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+ if (danishPrice < money){
+   console.log("danish khamu");
+ }
+ else if(butterBread < money){
+   console.log("butterBread khamu");
+
+ }
+ else if(toastBiscuit < money){
+   console.log("toastBiscuit khamu");
+
+ }
+ else{
+   console.log("khali cha khamu tonga boisa");
+ }
