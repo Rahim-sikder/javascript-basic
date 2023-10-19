a26662c2c2ab888a8fed7f7388d5207ea4711bf7
Rahim sikder <arsikder323@gmail.com>
2023-10-20 00:04:14 +0600

    summary done

---
+++ summary.js
@@ -0,0 +1,8 @@
+var destinations = ['nepal', 'india','kathmandu','bali','sp'];
+destinations[1]='bangladesh';
+console.log(destinations);
+var indexPosition  = destinations.indexOf('bali');
+console.log(indexPosition);
+console.log(destinations.length);
+destinations.push('usa');
+destinations.pop();
