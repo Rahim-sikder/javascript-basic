c93eae9727d98893d40f8cba0d45feab54b0d7e0
Rahim sikder <arsikder323@gmail.com>
2023-10-19 23:43:35 +0600

    nested done

---
+++ nested.js
@@ -0,0 +1,20 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+ if (math == true){
+   if(geometry == true){
+   
+      if(straightLine == true){
+     }
+      else{
+         console.log("straigtLine a ami cholbo na");
+     }
+}
+   else{
+      console.log("pithagorous parba na tahole");
+   }
+ }
+ else{
+
+ }
