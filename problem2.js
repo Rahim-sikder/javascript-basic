56dd8352abdd4f9aba08f917de9877bc03653b71
Rahim sikder <arsikder323@gmail.com>
2023-10-20 00:40:17 +0600

    problem2 done

---
+++ problem2.js
@@ -0,0 +1,25 @@
+
+var myScore = 85;
+var tomScore = 66;
+var janeScore = 95;
+var peterScore = 56;
+var jhonScore = 40;
+ 
+ if (myScore >= 80  ){
+    console.log("My grade is A");
+}
+ if( tomScore >= 60){
+   console.log("Tom grade is B");
+}
+ if( janeScore >= 95){
+   console.log("Jane grade is A");
+}
+ if( peterScore >= 40){
+   console.log("Peter grade is C");
+}
+ if( jhonScore >= 40){
+   console.log("Jhon grade is D");
+}
+else{
+   console.log("fail");
+}
