28100351a7a1400e7922570bb688cec8469486c6
Rahim sikder <arsikder323@gmail.com>
2023-10-21 02:30:04 +0600

    array-loop done

---
+++ array-loop.js
@@ -0,0 +1,15 @@
+// for (var i=0; i< 10; i++){
+//    console.log(i);
+// }
+
+// var numbers =[10, 20, 30,40,50,60,70];
+// for (var i=0; i < numbers.length; i++ ){
+//  var number = numbers [i];
+//    console.log(number);
+// }
+
+var items =['bottol', 'pen', 'paper','mouse','sunglass'];
+for(var i=0; i < items.length; i++){
+   var alItems = items[i];
+   console.log(alItems);
+}
