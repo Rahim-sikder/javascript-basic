318d4a07cfa8b354c6e53e7115eee793e65f05f0
Rahim sikder <arsikder323@gmail.com>
2023-10-22 04:01:09 +0600

    return clear

--- parameter.js
+++ parameter.js
@@ -2,4 +2,17 @@ function bringSingara (money){
    console.log("Mama singara dan");
    console.log(money);
 }
-bringSingara(20);
+// bringSingara(20);
+// function add (num1, num2){
+//    console.log('going to add:', num1, num2)
+
+// }
+//  add(125,200);
+function sum (a,b,c,d,e){
+   console.log(a,b,c,d,e);
+   var sum = a + b + c + d + e;
+   console.log(sum);
+
+
+}
+sum(1,2,3,4,5);
---
+++ return.js
@@ -0,0 +1,33 @@
+// function add (num1, num2){
+//    console.log(num1,num2);
+//    var sum = num1 + num2 ;
+//   return sum;
+// }
+// var total = add (80,70);
+// console.log('total:', total);
+
+// function sub (number1 , number2 ){
+//    console.log(number1, number2);
+//    var sub = number1 - number2;
+//    return sub;
+// }
+
+//  var total = sub (200 , 100);
+//  console.log('total sub', total);
+
+// function mult (number1 , number2){
+//    console.log (number1, number2);
+//    var mult = number1 / number2 ;
+//    return mult;
+// }
+// var total = (200, 100);
+// console.log('total sub', total);
+
+function bringSingara(money){
+    var  singaraPrice = 10;
+    var quantity =money / singaraPrice ;
+    return quantity;
+}
+var totatSingara = bringSingara(200);
+console.log('total singara:', totatSingara)
+
