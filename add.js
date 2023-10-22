cc2a98b038c05fb89e77842629d2e7786fc01a40
Rahim sikder <arsikder323@gmail.com>
2023-10-23 02:30:45 +0600

    add done

---
+++ add.js
@@ -0,0 +1,10 @@
+function add(num1 , num2){
+  const sum = num1 + num2;
+   return sum;
+
+}
+const result1 = add (70 ,50);
+const result2 = add (60 ,50);
+const result3 = add (80 ,50);
+const finalResult = add (result1, result2,result3 );
+console.log(finalResult);
