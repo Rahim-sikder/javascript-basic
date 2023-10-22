f9f93452a46f70292b5038ee139413c69096a9bc
Rahim sikder <arsikder323@gmail.com>
2023-10-23 02:42:05 +0600

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+const student ={
+   id:11183103,
+    name:'rahim',
+    class:1,
+    marks:98
+}
+
+var mobile = {
+   brand :'samsung',
+   color :'black',
+   ram_size :' 128gb',
+   price : 20000
+};
